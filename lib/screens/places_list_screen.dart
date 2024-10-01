362a37ac3d9945e1466a975f85dc26b80a7c36e4
adogbeji <baka_1993@yahoo.co.uk>
2024-10-01 11:03:35 +0100

    Add starting code for places list screen

---
+++ lib/screens/places_list_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class PlacesListScreen extends StatelessWidget {
+  const PlacesListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Places List Screen'),
+      ),
+    );
+  }
+}
