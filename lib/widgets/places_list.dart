c448c519fd1991713d2c84f689639cc21f44ddbd
adogbeji <baka_1993@yahoo.co.uk>
2024-10-02 10:48:14 +0100

    Initialise variable in PlacesList widget

--- lib/widgets/places_list.dart
+++ lib/widgets/places_list.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
+import 'package:favourite_places/models/place.dart';
+
 class PlacesList extends StatelessWidget {
-  const PlacesList({super.key});
+  const PlacesList({super.key, required this.places});
+
+  final List<Place> places;
 
   @override
   Widget build(BuildContext context) {
