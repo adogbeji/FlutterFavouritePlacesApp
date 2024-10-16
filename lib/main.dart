1913e8cffb96558bc58624cbb9c0179be4b717aa
adogbeji <baka_1993@yahoo.co.uk>
2024-10-16 10:44:25 +0100

    Create user places provider & use ProviderScope in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:favourite_places/screens/places_list_screen.dart';
 
@@ -29,7 +30,7 @@ final theme = ThemeData().copyWith(
 
 void main() {
   runApp(
-      const MyApp(),
+    const ProviderScope(child: MyApp(),),
   );
 }
 
