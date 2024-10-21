306c9b88d5e277fe0cfe451e7c3b36a578b498be
adogbeji <baka_1993@yahoo.co.uk>
2024-10-21 10:42:31 +0100

    Add starting code for place detail screen

---
+++ lib/screens/place_detail.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class PlaceDetailScreen extends StatelessWidget {
+  const PlaceDetailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Place Detail Screen'),
+      ),
+    );
+  }
+}
